0f9a035e187d0b506eb6d2b289880f666279d8ab
max-barabash <48650035+max-barabash@users.noreply.github.com>
2019-05-14 20:40:07 +0400

    Add routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import LoginPage from './containers/LoginPage';
+import NotFound from './components/NotFound'
+import TweetPage from './containers/TweetPage';
+import ProfilePage from './containers/ProfilePage';
 
 function App() {
   return (
-    <div>
-      SPA App
-    </div>
-  );
+        <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={LoginPage} />
+                <Route path="/profile" component={ProfilePage} />
+                <Route path="/tweets" component={TweetPage} />
+                <Route component={NotFound} />
+              </Switch>
+        </BrowserRouter>
+  )
 }
 
 export default App;
